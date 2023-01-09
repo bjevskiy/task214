50bdfcd2ae7a424d454f0f9ae227c8c25f3aa638
bjevskiy <nto.vtisa@gmail.com>
2023-01-09 06:55:54 +0300

    214

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,13 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
 
     @Override
     public String toString() {
-        return ", СЃРјРµСЂС‚СЊ РљРѕС‰РµСЏ РЅР° РёРіР»Рµ :( " + "";
+        return ", смерть Кощея на игле :( " + "";
     }
 }
